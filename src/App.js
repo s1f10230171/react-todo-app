06f1fd834d601908f50f5d90a79f5511dd98b062
1F10230171 <s1f102301718@iniad.org>
2025-05-02 04:30:00 +0900

    Todoアプリのタスク追加機能

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-
+import { useState } from 'react';
 import './App.css';
 import TodoInput from './TodoInput';
 import TodoList from './TodoList';
 
 function App() {
-  const todos = [
+  const createId = () => Math.random().toString(36).substring(2);
+  const initialTodos = [
     {
+      id: createId(),
       text: "React勉強",
       isFinished: true
     },
     {
+      id: createId(),
       text: "Java勉強",
       isFinished: false
     }
   ];
+  const [todos, setTodos] = useState(initialTodos);
+  const handleAdd = (text) => {
+    const newTodo = {
+      id: createId(),
+      text: text,
+      isFinished: false
+    };
+    setTodos((todos) => [...todos, newTodo]);
+  }
   return (
     <div className="App">
       <div className='App-title'>ToDo管理アプリ</div>
       <div className="App-content">
-        <TodoInput />
+        <TodoInput onAdd={handleAdd} />
         <TodoList todos={todos}/>
       </div>
     </div>
--- src/TodoInput.js
+++ src/TodoInput.js
@@ -1,4 +1,6 @@
-const TodoInput = () => {
+import { useState } from "react"
+
+const TodoInput = ({onAdd}) => {
     const formStyle = {
         display: 'flex',
     }
@@ -12,9 +14,18 @@ const TodoInput = () => {
         color: 'white', fontSize: 16, cusor: 'pointer', 
         paddingLeft: 15, paddingRight: 15
     }
+
+    const [task, setTask] = useState('');
+    const handleChange = (event) => {setTask(event.target.value);}
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        if(task === '') return;
+        onAdd(task);
+        setTask(''); }
     return(
-        <form style={formStyle}>
-            <input style={inputStyle} placeholder="タスクを入力"></input>
+        <form style={formStyle} onSubmit={handleSubmit}>
+            <input style={inputStyle} placeholder="タスクを入力"
+            type="text" onChange={handleChange} value={task}></input>
             <button style={btnStyle} type="submit">追加</button>
         </form>
     );
