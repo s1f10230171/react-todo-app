91da8b745a1f5084cd03147d3467440090f946ba
1F10230171 <s1f102301718@iniad.org>
2025-05-02 15:50:33 +0900

    タスク数の可視化

--- src/App.js
+++ src/App.js
@@ -19,16 +19,20 @@ function App() {
   ];
 
   const [todos, setTodos] = useState(initialTodos);
+  const [taskcount, setTaskcount] = useState(0)
+  const [clearcount, setClearcount] = useState(0)
   const handleAdd = (text) => {
     const newTodo = {
       id: createId(),
       text: text,
       isFinished: false
     };
+    setTaskcount(taskcount+1);
     setTodos((todos) => [...todos, newTodo]);
   }
   const handleRemove = (deleteId) => {
-      setTodos(todos.filter(todo => todo.id !== deleteId));
+    setClearcount(clearcount+1);
+    setTodos(todos.filter(todo => todo.id !== deleteId));
     }
   const handleToggle = (todoId) => {
       setTodos(todos.map(todo => todo.id === todoId ? {...todo, isFinished : !todo.isFinished } : todo));
@@ -39,6 +43,7 @@ function App() {
       <div className="App-content">
         <TodoInput onAdd={handleAdd} />
         <TodoList todos={todos} onRemove={handleRemove} onToggle={handleToggle}/>
+        <p>{taskcount===clearcount ? `ALL CLEAR!! タスク数${taskcount}` : `今週のタスクカウント: ${taskcount} / クリア済み : ${clearcount}`}</p>
       </div>
     </div>
   );
