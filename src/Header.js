6ac7560c51be4fff5db46f325951da7fb8cf3b5f
1F10230171 <s1f102301718@iniad.org>
2025-05-08 01:06:20 +0900

    navの構成をブートストラップに変更

--- src/Header.js
+++ src/Header.js
@@ -2,35 +2,55 @@ import React, { useState } from 'react'
 import {Link} from "react-router-dom";
 
 function Header() {
-    const [isOpen, setIsOpen] = useState(false);
-    const toggleContents = () => {
-            setIsOpen(!isOpen);
-    }
+    // const [isOpen, setIsOpen] = useState(false);
+    // const toggleContents = () => {
+    //         setIsOpen(!isOpen);
+    // }
   return (
-    <div>
-        <div className="App-title row">
-            <div className="col-10">
-                <h2 className='ms-3'>ToDo管理アプリ</h2>
-            </div>
-            <div className="col-2">
-            <button onClick={toggleContents}>{isOpen ? <i className="bi bi-x-lg"></i>: <i className="bi bi-list"></i>}</button>
+    <nav className="navbar navbar-dark bg-dark">
+        <div className="container">
+            <a className="navbar-brand">ToDo管理アプリ</a>
+            <button className='navbar-toggler' type="button" data-bs-toggle="collapse" data-bs-target="#navbarContent" aria-controls="navbarContent" aria-expanded="false" aria-label='Toggle navigation'>
+                <span className='navbar-toggler-icon'></span>
+            </button>
+            <div className='collapse navbar-collapse' id="navbarContent">
+                <ul className='navbar-nav mb-2'>
+                    <li className='nav-item'>
+                        <Link to="/">ホーム</Link>
+                    </li>
+                </ul>
+                <ul className='navbar-nav mb-2'>
+                    <li className='nav-item'>
+                        <Link to="/history">履歴</Link>
+                    </li>
+                </ul>
             </div>
         </div>
-    {isOpen &&(
-    <header>
-        <nav>
-            <ul>
-                <li>
-                    <Link to="/">ホーム</Link>
-                </li>
-                <li>
-                    <Link to="/history">履歴</Link>
-                </li>
-            </ul>
-        </nav>
-    </header>
-  )}
-    </div>
+    </nav>
+    // <div>
+    //     <div className="App-title row">
+    //         <div className="col-10">
+    //             <h2 className='ms-3'>ToDo管理アプリ</h2>
+    //         </div>
+    //         <div className="col-2">
+    //         <button onClick={toggleContents}>{isOpen ? <i className="bi bi-x-lg"></i>: <i className="bi bi-list"></i>}</button>
+    //         </div>
+    //     </div>
+    // {isOpen &&(
+    // <header>
+    //     <nav>
+    //         <ul>
+    //             <li>
+    //                 <Link to="/">ホーム</Link>
+    //             </li>
+    //             <li>
+    //                 <Link to="/history">履歴</Link>
+    //             </li>
+    //         </ul>
+    //     </nav>
+    // </header>
+//   )}
+//     </div>
   )
 };
 
