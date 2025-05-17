1f88d1f86e575fe6c15d1c13b186830c07f47d54
shuei-oshikubo <shuei_oshikubo@reazon.jp>
2025-05-17 16:26:34 +0900

    不要な呼び出しを削除

--- src/App.js
+++ src/App.js
@@ -1,27 +1,13 @@
-import React, { useState, useEffect } from 'react';
-import { useTodos } from './hooks/useTodos';
+import React from 'react';
 import './App.css';
-import TodoInput from './components/TodoInput';
-import TodoList from './components/TodoList';
-import MyChart from './components/MyChart';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import History from "./components/History";
 import Header from './components/Header';
-import StackedImages from './components/StackedImage';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { getThisWeekClearCount } from './utils/utils';
 import HomePage from './pages/HomePage';
 import HistoryPage from './pages/HistoryPage';
 
 function App() {
-
-  const { todos,  taskcount, clearcount, handleAdd, handleRemove, handleToggle } = useTodos();
-  const [historyData, setHistoryData] = useState({});
-
-  useEffect(() => {
-    setHistoryData(JSON.parse(localStorage.getItem("todosByDate")) || {});
-  }, [todos]);
 
   return (
     <Router>
